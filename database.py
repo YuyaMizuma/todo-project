b8fc58601f726924396331e45c6b6b81f066abfb
YuyaMizuma <mizuma.yuya.2003@gmail.com>
2025-07-30 17:57:42 +0900

    renderでのエラーを修正

--- database.py
+++ database.py
@@ -1,12 +1,20 @@
+import os # osをインポート
 from sqlmodel import create_engine, SQLModel
 
-#データベースファイルの名前を指定
-sqlite_file_name = "database.sqlite"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+# Renderが提供するDATABASE_URLを読み込む。なければNoneになる
+database_url = os.getenv("DATABASE_URL")
 
-#データベースに接続するためのエンジンを作成
-engine = create_engine(sqlite_url, echo=True)
+# もしDATABASE_URLがなければ（ローカル環境）、SQLiteを使う
+if not database_url:
+    sqlite_file_name = "database.sqlite"
+    sqlite_url = f"sqlite:///{sqlite_file_name}"
+    # SQLiteを使う場合に必要な設定
+    connect_args = {"check_same_thread": False}
+    engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+else:
+    # DATABASE_URLがあれば（Render環境）、PostgreSQLに接続
+    engine = create_engine(database_url, echo=True)
+
 
 def create_db_and_tables():
-    # model.pyで定義したモデルに基づいてテーブルを作成
     SQLModel.metadata.create_all(engine)
